c7ee73196f0ba9b5f7934b991943d4d820297b4b
xelaSeventh <afx.xela@gmail.com>
2024-01-15 22:02:51 -0600

    Inventory Microservice

---
+++ microservices-parent/inventory-service/src/main/java/dev/xelaseventh/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,20 @@
+package dev.xelaseventh.inventoryservice.controller;
+
+import dev.xelaseventh.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+        return inventoryService.isInStock(skuCode);
+    }
+}
---
+++ microservices-parent/inventory-service/src/test/java/dev/xelaseventh/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.xelaseventh.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
